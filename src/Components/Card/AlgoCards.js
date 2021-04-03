1eb08e12ee1a48d28b67989e2d1cfae51908b79d
Netanel Mazuz <netazuz@gmail.com>
2021-04-03 14:50:17 +0300

    update

--- src/Components/Card/AlgoCards.js
+++ src/Components/Card/AlgoCards.js
@@ -3,13 +3,24 @@ import Card from './Card';
 
 export default class AlgoCards extends Component {
   state = {
-
+    images: this.importAll(require.context('../../img', false, '/.gif/'))
   }
+  
+  importAll = (r) => {
+    let images = {};
+    r.keys().map((item, index) => images[item.replace('./', '')] = r(item));
+    return images;
+  }
+  
   render() {
     return (
-      <div className="algo-cards-container">
-        <Card/>
-      </div>
+      <main className="algo-cards-container">
+        <ul className="algo-list">
+          <li className="card-viz">
+            <Card/>
+          </li>  
+        </ul>
+      </main>
     )
   }
 }
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Card extends Component {
+  
+
   render() {
+    
     return (
       <div className="algo-card">
-        A
+        <a href="/">
+          <img src="" alt="please wait" width="240.297px" height="240.438px"></img>
+        </a>
       </div>
     )
   }
